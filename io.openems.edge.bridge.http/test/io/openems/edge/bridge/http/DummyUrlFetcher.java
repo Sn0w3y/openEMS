5b0b69e8831ce3ba74c6e04d61375296d974dd25
Hannes <hallerjo97@gmail.com>
2024-03-30 18:06:49 +0100

    Update DummyUrlFetcher.java

--- io.openems.edge.bridge.http/test/io/openems/edge/bridge/http/DummyUrlFetcher.java
+++ io.openems.edge.bridge.http/test/io/openems/edge/bridge/http/DummyUrlFetcher.java
@@ -47,4 +47,14 @@ public class DummyUrlFetcher implements UrlFetcher {
 		this.onTaskFinished = onTaskFinished == null ? EMPTY_RUNNABLE : onTaskFinished;
 	}
 
+	@Override
+	public byte[] fetchEndpointRaw(Endpoint endpoint) throws OpenemsNamedException {
+	    // Simulated response data
+	    byte[] simulatedResponse = "simulated response".getBytes();
+	    // Optionally, trigger any task finished actions.
+	    this.onTaskFinished.run();
+	    return simulatedResponse;
+	}
+
+
 }
